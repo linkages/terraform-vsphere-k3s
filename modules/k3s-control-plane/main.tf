e5aa33e4199e191d22d8a2498ae451d28fe76318
Eli Ben-Shoshan <eli@benshoshan.com>
2021-06-02 08:37:36 -0400

    Upgrade to using k3s version 1.21.1+k3s1 since we need support for cgroupsv2

--- modules/k3s-control-plane/main.tf
+++ modules/k3s-control-plane/main.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "control_plane_first_node" {
   provisioner "remote-exec" {
     inline = [
-      "curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='server --write-kubeconfig-mode 644 --cluster-init --token ${var.k3s_token} --tls-san 1${var.lb_address}' INSTALL_K3S_VERSION='v1.19.1+k3s1' sh -"
+      "curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='server --write-kubeconfig-mode 644 --cluster-init --token ${var.k3s_token} --tls-san ${var.lb_address}' INSTALL_K3S_VERSION='v1.21.1+k3s1' sh -"
     ]
 
     connection {
@@ -26,7 +26,7 @@ resource "null_resource" "control_plane_subsequent_nodes" {
   count = length(var.subsequent_control_plane_node_ips)
   provisioner "remote-exec" {
     inline = [
-      "curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--write-kubeconfig-mode 644 --server https://${var.first_control_plane_node_ip}:6443 --token ${var.k3s_token}' INSTALL_K3S_VERSION='v1.19.1+k3s1' sh -"
+      "curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC='--write-kubeconfig-mode 644 --server https://${var.first_control_plane_node_ip}:6443 --token ${var.k3s_token}' INSTALL_K3S_VERSION='v1.21.1+k3s1' sh -"
     ]
 
     connection {
