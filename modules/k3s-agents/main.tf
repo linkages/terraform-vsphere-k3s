01312bfeff3df88e22e3b1980bf83bfb22244fec
Eli Ben-Shoshan <eli@benshoshan.com>
2021-06-02 08:37:53 -0400

    Upgrade to using k3s version 1.21.1+k3s1 since we need support for cgroupsv2

--- modules/k3s-agents/main.tf
+++ modules/k3s-agents/main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "agent_nodes" {
   count = length(var.agent_node_ips)
   provisioner "remote-exec" {
     inline = [
-      "curl -sfL https://get.k3s.io | K3S_URL=https://${var.first_control_plane_node_ip}:6443 K3S_TOKEN=${var.k3s_token} INSTALL_K3S_VERSION='v1.19.1+k3s1' sh -"
+      "curl -sfL https://get.k3s.io | K3S_URL=https://${var.first_control_plane_node_ip}:6443 K3S_TOKEN=${var.k3s_token} INSTALL_K3S_VERSION='v1.21.1+k3s1' sh -"
     ]
 
     connection {
